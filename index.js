c55339c0d1e74fbcfddde6910d3452862b3b3c41
Americo Childs <americochilds@gmail.com>
2021-09-28 00:32:28 -0500

    finished portfolio

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var path = __dirname + "/";
 var router = express.Router();
 
 app.use('/', router);
-
+console.log("bruh");
 app.use(express.static('public'));
 //
 router.get('/', function(req, res) {
